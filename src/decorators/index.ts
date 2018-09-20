7a3802228c6513f3e74b73a9eb2863460952b1c5
ashleigh <ashleigh@wi-q.com>
2018-09-20 15:16:00 +0100

    Linting issues apparently :shrug:

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -3,4 +3,4 @@ import { Inject } from '@nestjs/common';
 
 export const InjectAmqpConnection = (name: string = 'default') => {
   return Inject(createConnectionToken(name));
-}
+};
--- src/utils/create.tokens.ts
+++ src/utils/create.tokens.ts
@@ -5,8 +5,8 @@ import {
 
 export const createOptionsToken = (name: string): string => {
   return `${AMQP_OPTIONS_PROVIDER}_${name}`;
-}
+};
 
 export const createConnectionToken = (name: string): string => {
   return `${AMQP_CONNECTION_PROVIDER}_${name}`;
-}
+};
