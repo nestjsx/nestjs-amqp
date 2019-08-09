4076d81d59c3e69207cf05b422ddd71a5fb8f682
Ashleigh <ashleigh.simonelli@velocityapp.com>
2019-08-09 14:39:10 +0100

    Added option to avoid promise return

--- src/interfaces/amqp.async.options.ts
+++ src/interfaces/amqp.async.options.ts
@@ -2,7 +2,7 @@ import { AmqpOptionsInterface } from './amqp.options';
 
 export interface AmqpAsyncOptionsInterface {
   inject?: any[];
-  useFactory?: (...args: any[]) => Promise<AmqpOptionsInterface>;
+  useFactory?: (...args: any[]) => Promise<AmqpOptionsInterface> | AmqpAsyncOptionsInterface;
   retrys?: number;
   retryDelay?: number;
 }
